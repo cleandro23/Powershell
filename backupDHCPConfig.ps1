7fae73d06e009795823a33e8346ed49f7dbb9def
crcnn <CRCNN@coloplast.com>
2023-09-08 14:20:09 -0600

    Functions and structure documentation

--- backupDHCPConfig.ps1
+++ backupDHCPConfig.ps1
@@ -3,8 +3,8 @@ function CreateDhcpDump{
   [CmdletBinding()]
   Param([string]$dateFolder)
 
-  $dumpPath = Join-Path "\\LTCRSJO00182\c$\dumpf" $dateFolder
-  mkdir $dumpPath
+  $dumpPath = Join-Path "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\" $dateFolder
+  #mkdir $dumpPath
 }
 
 
@@ -13,7 +13,7 @@ function Export-DhcpConfigurations {
 
   foreach ($exportConfig in $exportConfigList) {
       Write-Host "Exporting DHCP configuration for $exportConfig..."
-      $filePath = Join-Path "\\LTCRSJO00182\C$\dumpf\$dateFolder" "$exportConfig.xml"
+      $filePath = Join-Path "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\dumpf\$dateFolder" "$exportConfig.xml"
       Export-DhcpServer -ErrorAction 'SilentlyContinue' -ComputerName $exportConfig -File $filePath -Force -Leases
   }
 }
@@ -21,34 +21,33 @@ function Export-DhcpConfigurations {
 # Main script
 $dateFolder = Get-Date -Format "yyyyMMdd"
 
-
-
 # Import AD Module
  Import-Module ActiveDirectory
 
- # Realiza una consulta en Active Directory para obtener los servidores
+ # Query to find the servers due to parameters, for this case, I'll use GI in terms to find DHCP
  $servers = Get-ADComputer -Filter {Name -like "*GI*"} | Select-Object -ExpandProperty Name
  
- # Verifica si se encontraron servidores
+ # This loop will 
  if ($servers.Count -gt 0) {
-     # Guarda la lista de servidores en un archivo de texto
+     # Save servers information to the specific folder
      $servers | Out-File -FilePath "C:\dumpf\servers.txt"
-     Write-Host "Se ha guardado la lista de servidores en el archivo servers.txt"
+     Write-Host "Server list has been saved in the following address \\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\servers.txt"
  } else {
-     Write-Host "No se encontraron servidores en Active Directory."
+     Write-Host "No servers were found"
  }
  
 
 
-#ruta del archivo
-$serverFile =  "C:\dumpf\servers.txt"
+#File addres
+$serverFile =  "D:PLKRGO_GI_DHCP_dump_test\servers.txt\servers.txt"
 
-# Leer contenido del archivo
+# Read content of the TXT File
 $fileContent = Get-Content $serverFile
 
-# Crear lista de archivos 
+# Server list creation using the TXT file 
 $serverList = $fileContent | ForEach-Object { $_.Trim() }
 
+#Export the .xml files, using the information contained in the TXT file
 $exportConfigList = @(
   $serverList
 )
