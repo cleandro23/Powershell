7c44d5f515ba5b4476161f64e83dcefb06e2ef0e
crcnn <CRCNN@coloplast.com>
2023-09-13 16:08:36 -0600

    Final docuementation

--- backupDHCPConfig.ps1
+++ backupDHCPConfig.ps1
@@ -1,10 +1,14 @@
-
+# Name        : backupDHCPConfig,ps1
+# Description : DHCP servers backup
+# Version     : 1.0.0
+# Author      : Carlos Navarro, CRCNN
+# Date        : 12/09/23
 function CreateDhcpDump{
   [CmdletBinding()]
   Param([string]$dateFolder)
 
   $dumpPath = Join-Path "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\" $dateFolder
-  #mkdir $dumpPath
+  mkdir $dumpPath
 }
 
 
@@ -13,7 +17,7 @@ function Export-DhcpConfigurations {
 
   foreach ($exportConfig in $exportConfigList) {
       Write-Host "Exporting DHCP configuration for $exportConfig..."
-      $filePath = Join-Path "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\dumpf\$dateFolder" "$exportConfig.xml"
+      $filePath = Join-Path "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\$dateFolder" "$exportConfig.xml"
       Export-DhcpServer -ErrorAction 'SilentlyContinue' -ComputerName $exportConfig -File $filePath -Force -Leases
   }
 }
@@ -30,7 +34,7 @@ $dateFolder = Get-Date -Format "yyyyMMdd"
  # This loop will 
  if ($servers.Count -gt 0) {
      # Save servers information to the specific folder
-     $servers | Out-File -FilePath "C:\dumpf\servers.txt"
+     $servers | Out-File -FilePath "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\servers.txt"
      Write-Host "Server list has been saved in the following address \\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\servers.txt"
  } else {
      Write-Host "No servers were found"
@@ -39,7 +43,7 @@ $dateFolder = Get-Date -Format "yyyyMMdd"
 
 
 #File addres
-$serverFile =  "D:PLKRGO_GI_DHCP_dump_test\servers.txt\servers.txt"
+$serverFile =  "\\GICPSRV02\D$\PLKRGO_GI_DHCP_dump_test\servers.txt"
 
 # Read content of the TXT File
 $fileContent = Get-Content $serverFile
@@ -53,5 +57,4 @@ $exportConfigList = @(
 )
 
 CreateDhcpDump $dateFolder
-importServers $servers
 Export-DhcpConfigurations -dateFolder $dateFolder -exportConfigList $exportConfigList
