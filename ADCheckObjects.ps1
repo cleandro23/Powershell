d2b9e98e36eaa12af997cc09710b2476f8d013db
cleandro23 <36335058+cleandro23@users.noreply.github.com>
2024-05-29 14:02:55 -0600

    AD Obj Deletion, Log Creation
    Code for objects deletion and log creation base on user executor

--- ADCheckObjects.ps1
+++ ADCheckObjects.ps1
@@ -1,28 +1,48 @@
 # Name        : ADCheckObjects.ps1
 # Description : Check in AD all old stuff with no login
-# Version     : 1.0.0
+# Version     : 1.2.0
 # Author      : Carlos Navarro, CRCNN
-# Date        : 27/05/2027
+# Date        : 27/05/2024
 
 
-# Importar el módulo Active Directory
+# Import AD Module
 Import-Module ActiveDirectory
 
-# Definir la fecha límite de inactividad
-$inactivityDate = (Get-Date).AddYears(-5)
+# CSV Address to be save
+$csvPath = "C:\Temp\InactiveObjects.csv"
+$logPath = "C:\Temp\InactiveObjects.txt"
 
-# Obtener usuarios que no han iniciado sesión en los últimos 10 años
+# Log Information
+# Get the executor user 
+$executingUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
+
+# Log preparation
+if (Test-Path $logPath) {
+    Remove-Item $logPath
+}
+New-Item -Path $logPath -ItemType File
+
+# Escribir información del usuario que ejecutó el script en el log
+Add-Content -Path $logPath -Value "Script executed by: $executingUser"
+Add-Content -Path $logPath -Value "Execution date: $(Get-Date)"
+Add-Content -Path $logPath -Value ""
+
+# Define how many years you wanna search
+$inactivityDate = (Get-Date).AddYears(-10)
+
+# Get user object with the defined "lastlogon"
 $staleUsers = Get-ADUser -Filter {LastLogonTimestamp -lt $inactivityDate} -Properties LastLogonTimestamp | 
               Select-Object Name, SamAccountName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
 
-# Obtener computadoras que no han sido usadas en los últimos 10 años
+# Get computer object with the defined "lastlogon"
 $staleComputers = Get-ADComputer -Filter {LastLogonTimestamp -lt $inactivityDate} -Properties LastLogonTimestamp | 
                   Select-Object Name, SamAccountName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
 
-# Preparar resultados para exportar
+#  Prepare results to export
 $results = @()
+$deletedObjects = @()
 
-# Procesar usuarios inactivos
+# Inactive users process
 $staleUsers | ForEach-Object {
     $lastLogonDate = $_.LastLogonDate
     if ($lastLogonDate -eq [DateTime]::MinValue) {
@@ -36,7 +56,12 @@ $staleUsers | ForEach-Object {
     }
 }
 
-# Procesar computadoras inactivas
+# Remove user and register the action
+Remove-ADObject -Identity $_.DistinguishedName -Confirm:$false
+$deletedObjects += "User: $($_.Name), SamAccountName: $($_.SamAccountName), LastLogonDate: $lastLogonDate, DistinguishedName: $($_.DistinguishedName)"
+
+
+# Inactive computer process
 $staleComputers | ForEach-Object {
     $lastLogonDate = $_.LastLogonDate
     if ($lastLogonDate -eq [DateTime]::MinValue) {
@@ -50,8 +75,20 @@ $staleComputers | ForEach-Object {
     }
 }
 
-# Exportar los resultados a un archivo CSV
-$results | Export-Csv -Path "C:\Temp\objetos_inactivos.csv" -NoTypeInformation -Encoding UTF8
+#Remove computer or server objetc and register the action
+#Remove-ADObject -Identity $_.DistinguishedName -Confirm:$false
+#$deletedObjects += "Computer: $($_.Name), SamAccountName: $($_.SamAccountName), LastLogonDate: $lastLogonDate, DistinguishedName: $($_.DistinguishedName)"
 
-# Confirmar que el script se completó
-Write-Output "Script completado. Los resultados se han guardado en C:\Temp\objetos_inactivos.csv"
+
+# Export the results to CSV
+$results | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+
+# Removed object log
+#$deletedObjects | Out-File -FilePath $logPath -Encoding UTF8
+
+# Send mail
+Send-MailMessage -from mail -to mail -Subject "Attached you will find the Inactive Objects" -Priority High -Body "Please check attached log file" -SmtpServer smtp.cpcorp.net -Attachments $csvPath
+
+
+# Comfirmation of mail
+Write-Output "Script completed. The results have been saved to $csvPath and sent by email."
