b1329a3e622474fe565bf913e6ddae77c7e4af70
cleandro23 <36335058+cleandro23@users.noreply.github.com>
2024-06-03 14:45:28 -0600

    AD Cred Integration
    Integration of AD User and credentials to delete objects in AD

--- ADCheckObjects.ps1
+++ ADCheckObjects.ps1
@@ -1,6 +1,6 @@
 # Name        : ADCheckObjects.ps1
 # Description : Check in AD all old stuff with no login
-# Version     : 1.2.0
+# Version     : 1.3.0
 # Author      : Carlos Navarro, CRCNN
 # Date        : 27/05/2024
 
@@ -9,85 +9,85 @@
 Import-Module ActiveDirectory
 
 # CSV Address to be save
-$csvPath = "C:\Temp\InactiveObjects.csv"
-$logPath = "C:\Temp\InactiveObjects.txt"
+$testDate = Get-Date -date $(get-date) -format ddMMyyyy
+$csvPath = "C:\Temp\InactiveObjects_$testDate.csv"
+$logPath = "C:\Temp\InactiveObjects_$testDate.txt"
+
 
 # Log Information
 # Get the executor user 
 $executingUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 
-# Log preparation
+# Preparar el log
 if (Test-Path $logPath) {
     Remove-Item $logPath
 }
 New-Item -Path $logPath -ItemType File
 
 # Escribir información del usuario que ejecutó el script en el log
-Add-Content -Path $logPath -Value "Script executed by: $executingUser"
-Add-Content -Path $logPath -Value "Execution date: $(Get-Date)"
+Add-Content -Path $logPath -Value "Script ejecutado por: $executingUser"
+Add-Content -Path $logPath -Value "Fecha de ejecución: $(Get-Date)"
 Add-Content -Path $logPath -Value ""
 
-# Define how many years you wanna search
-$inactivityDate = (Get-Date).AddYears(-10)
-
-# Get user object with the defined "lastlogon"
+# Obtener usuarios que no han iniciado sesión en los últimos 10 años
 $staleUsers = Get-ADUser -Filter {LastLogonTimestamp -lt $inactivityDate} -Properties LastLogonTimestamp | 
-              Select-Object Name, SamAccountName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
+              Select-Object Name, SamAccountName, DistinguishedName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
 
-# Get computer object with the defined "lastlogon"
+# Obtener computadoras que no han sido usadas en los últimos 10 años
 $staleComputers = Get-ADComputer -Filter {LastLogonTimestamp -lt $inactivityDate} -Properties LastLogonTimestamp | 
-                  Select-Object Name, SamAccountName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
+                  Select-Object Name, SamAccountName, DistinguishedName, LastLogonTimestamp, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}
 
-#  Prepare results to export
+# Preparar resultados para exportar
 $results = @()
 $deletedObjects = @()
 
-# Inactive users process
-$staleUsers | ForEach-Object {
-    $lastLogonDate = $_.LastLogonDate
-    if ($lastLogonDate -eq [DateTime]::MinValue) {
-        $lastLogonDate = "Never"
+# Credenciales para la eliminación
+$credUser = "ADUser"  # Nombre de usuario para las credenciales de AD
+$credPassword = ConvertTo-SecureString "ADPassword" -AsPlainText -Force  # Contraseña
+$credential = New-Object System.Management.Automation.PSCredential($credUser, $credPassword)
+
+# ScriptBlock para eliminar objetos
+$deleteScriptBlock = {
+    param ($userOrComputer)
+
+    Import-Module ActiveDirectory
+
+    foreach ($item in $userOrComputer) {
+        $lastLogonDate = $item.LastLogonDate
+        if ($lastLogonDate -eq [DateTime]::MinValue) {
+            $lastLogonDate = "Never"
+        }
+        
+        $objectType = if ($item.ObjectClass -eq 'user') { "User" } else { "Computer" }
+        $results += [PSCustomObject]@{
+            ObjectType = $objectType
+            Name = $item.Name
+            SamAccountName = $item.SamAccountName
+            LastLogonDate = $lastLogonDate
+        }
+
+        # Eliminar el objeto y registrar el borrado
+        Remove-ADObject -Identity $item.DistinguishedName -Confirm:$false
+        $deletedObjects += "$objectType : $($item.Name), SamAccountName: $($item.SamAccountName), LastLogonDate: $lastLogonDate, DistinguishedName: $($item.DistinguishedName)"
     }
-    $results += [PSCustomObject]@{
-        ObjectType = "User"
-        Name = $_.Name
-        SamAccountName = $_.SamAccountName
-        LastLogonDate = $lastLogonDate
-    }
+
+    return $deletedObjects
 }
 
-# Remove user and register the action
-Remove-ADObject -Identity $_.DistinguishedName -Confirm:$false
-$deletedObjects += "User: $($_.Name), SamAccountName: $($_.SamAccountName), LastLogonDate: $lastLogonDate, DistinguishedName: $($_.DistinguishedName)"
+# Procesar y eliminar usuarios inactivos usando credenciales específicas
+Invoke-Command -Credential $credential -ScriptBlock $deleteScriptBlock -ArgumentList ($staleUsers)
 
+# Procesar y eliminar computadoras inactivas usando credenciales específicas
+Invoke-Command -Credential $credential -ScriptBlock $deleteScriptBlock -ArgumentList ($staleComputers)
 
-# Inactive computer process
-$staleComputers | ForEach-Object {
-    $lastLogonDate = $_.LastLogonDate
-    if ($lastLogonDate -eq [DateTime]::MinValue) {
-        $lastLogonDate = "Never"
-    }
-    $results += [PSCustomObject]@{
-        ObjectType = "Computer"
-        Name = $_.Name
-        SamAccountName = $_.SamAccountName
-        LastLogonDate = $lastLogonDate
-    }
-}
-
-#Remove computer or server objetc and register the action
-#Remove-ADObject -Identity $_.DistinguishedName -Confirm:$false
-#$deletedObjects += "Computer: $($_.Name), SamAccountName: $($_.SamAccountName), LastLogonDate: $lastLogonDate, DistinguishedName: $($_.DistinguishedName)"
-
-
-# Export the results to CSV
+# Exportar los resultados a un archivo CSV
 $results | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
 
-# Removed object log
-#$deletedObjects | Out-File -FilePath $logPath -Encoding UTF8
+# Guardar el log de los objetos borrados
+$deletedObjects | Out-File -FilePath $logPath -Encoding UTF8 -Append
 
 # Send mail
-Send-MailMessage -from mail -to mail -Subject "Attached you will find the Inactive Objects" -Priority High -Body "Please check attached log file" -SmtpServer smtp.cpcorp.net -Attachments $csvPath
+Send-MailMessage -from crcnn@coloplast.com -to crcnn@coloplast.com -Subject "Attached you will find the Inactive Objects" -Priority High -Body "Please check attached log file, this is a test from $(Get-Date)" -SmtpServer smtp.cpcorp.net -Attachments $csvPath
 
 
 # Comfirmation of mail
